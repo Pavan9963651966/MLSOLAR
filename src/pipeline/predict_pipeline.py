11fdf757258cb0b05d02f6347e82d781b66789ba
pavan kumar <pavan1122pkj@gmail.com>
2024-08-25 22:24:03 +0530

    prediction pipeline

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import sys
+from src.exception import CustomException
+from src.utils import load_object
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self,features):
+        try:
+            model_path=os.path.join("artifacts","model.pkl")
+            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            print("Before Loading")
+            model=load_object(file_path=model_path)
+            preprocessor=load_object(file_path=preprocessor_path)
+            print("After Loading")
+            data_scaled=preprocessor.transform(features)
+            preds=model.predict(data_scaled)
+            return preds
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+class CustomData:
+    def __init__(
+        self,
+        Time: float,
+        Ipv: float,
+        Vpv: float,
+        Vdc: float,
+        ia: float,
+        ib: float,
+        ic: float,
+        va: float,
+        vb: float,
+        vc: float,
+        Iabc: float,
+        If: float,
+        Vabc: float,
+        Vf: float
+    ):
+        self.Time = Time
+        self.Ipv = Ipv
+        self.Vpv = Vpv
+        self.Vdc = Vdc
+        self.ia = ia
+        self.ib = ib
+        self.ic = ic
+        self.va = va
+        self.vb = vb
+        self.vc = vc
+        self.Iabc = Iabc
+        self.If = If
+        self.Vabc = Vabc
+        self.Vf = Vf
+
+    def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict = {
+                "Time": [self.Time],
+                "Ipv": [self.Ipv],
+                "Vpv": [self.Vpv],
+                "Vdc": [self.Vdc],
+                "ia": [self.ia],
+                "ib": [self.ib],
+                "ic": [self.ic],
+                "va": [self.va],
+                "vb": [self.vb],
+                "vc": [self.vc],
+                "Iabc": [self.Iabc],
+                "If": [self.If],
+                "Vabc": [self.Vabc],
+                "Vf": [self.Vf],
+            }
+
+            return pd.DataFrame(custom_data_input_dict)
+
+        except Exception as e:
+            raise CustomException(e, sys)
--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,6 @@ import numpy as np
 import pandas as pd
 import dill
 import pickle
-from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import GridSearchCV
 
@@ -23,33 +22,33 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train, y_train, X_test, y_test, param):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
-        # Initialize the XGBoost model
-        model = XGBClassifier()
-        
-        # Initialize GridSearchCV with the XGBoost model and parameters
-        gs = GridSearchCV(model, param, cv=3, scoring='accuracy')  # Set scoring to accuracy
-        gs.fit(X_train, y_train)
+        report = {}
 
-        # Set the best parameters and refit the model
-        model.set_params(**gs.best_params_)
-        model.fit(X_train, y_train)
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-        # Make predictions
-        y_train_pred = model.predict(X_train)
-        y_test_pred = model.predict(X_test)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
-        # Calculate accuracy scores
-        train_model_score = accuracy_score(y_train, y_train_pred)
-        test_model_score = accuracy_score(y_test, y_test_pred)
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
 
-        # Return the accuracy scores and best parameters
-        return {
-            'train_accuracy': train_model_score,
-            'test_accuracy': test_model_score,
-            'best_params': gs.best_params_
-        }
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = accuracy_score(y_train, y_train_pred)
+
+            test_model_score = accuracy_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
 
     except Exception as e:
         raise CustomException(e, sys)
@@ -61,4 +60,3 @@ def load_object(file_path):
 
     except Exception as e:
         raise CustomException(e, sys)
-
